1d0168d13c823887ac41ef4b61433231bc242772
EvgenyBYT <fokin_ev@mail.ru>
2022-09-15 11:07:09 +0300

    game

---
+++ game_random_predict.py
@@ -0,0 +1,27 @@
+"""игра угадай число
+компьютер сам загадывает и угадывает число"""
+
+import numpy as np
+
+number = np.random.randint(1,101)
+print(f'компьютер загадал {number}')
+
+def random_predict():
+    
+    count = 0
+    num_max=101
+    num_min=1
+
+    while True:
+        count+=1
+        predict_number=np.random.randint(num_min, num_max)
+        if predict_number<number: 
+            num_min=predict_number
+        elif predict_number>number:
+            num_max=predict_number
+        elif predict_number==number:
+            print(f'Ваш код угадал число за {count} попыток, загаданное число {predict_number}')
+            break
+    return count
+
+random_predict()
