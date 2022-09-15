1cfcd17f9301072fc259ad8fe6d2a62eccf2aff1
EvgenyBYT <fokin_ev@mail.ru>
2022-09-15 12:00:44 +0300

    cod

--- game_random_predict.py
+++ game_random_predict.py
@@ -6,8 +6,15 @@ import numpy as np
 number = np.random.randint(1,101)
 print(f'компьютер загадал {number}')
 
-def random_predict():
-    
+def random_predict() -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number: Загаданное число. По умолчанию рандомно загадывается компьютером в диапазоне 1-100.
+
+    Returns:
+        Число попыток 
+    """
     count = 0
     num_max=101
     num_min=1
