5f8691c828c332d7e2cfa3f6b9f3c6d884266d9e
EvgenyBYT <fokin_ev@mail.ru>
2022-09-26 15:27:36 +0300

    2

--- temp.py
+++ temp.py
@@ -1,7 +1,15 @@
-import numpy as np
-
-simplelist = [19, 242, 14, 152, 142, 1000]
-
-
-print(np.mean(simplelist))   
-
+import pandas as pd
+countries_df = pd.DataFrame(
+    data = [
+        ['Англия', 56.29, 133396],
+        ['Канада', 38.05, 9984670],
+        ['США', 322.28, 9826630],
+        ['Россия', 146.24, 17125191],
+        ['Украина', 45.5, 603628],
+        ['Беларусь', 9.5, 207600],
+        ['Казахстан', 17.04, 2724902]
+    ],
+    columns= ['country', 'population', 'square'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ']
+)
+print(countries_df['RU',country])
